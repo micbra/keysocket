daf996e17fac2b8063672beca350af31603a7917
Valera Leontyev <feedbee@gmail.com>
2017-05-10 22:10:41 +0300

    Merge branch 'master' of https://github.com/borismus/keysocket
    # Conflicts:
    #	.gitignore
    #	README.md
    #	extension/keysocket-amazon-cloud-player.js
    #	extension/keysocket-grooveshark.js
    #	extension/keysocket-jango.js
    #	extension/keysocket-netflix.js
    #	extension/keysocket-vkontakte.js
    #	extension/manifest.json
    #	extension/shared.js
    #	pack.sh

--- extension/keysocket-picartotv.js
+++ extension/keysocket-picartotv.js
@@ -2,7 +2,7 @@ function onKeyPress(key) {
     if (key === PLAY) {
         var playPauseButtons = document.getElementsByClassName('vjs-play-control');
         for (var i = playPauseButtons.length - 1; i >= 0; i--) {
-        	simulateClick(playPauseButtons[i]);
+            simulateClick(playPauseButtons[i]);
         }
     }
 }
